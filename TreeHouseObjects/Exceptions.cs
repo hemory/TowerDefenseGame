a7c9c0e6feb3ae133b7dd50b797a2831dab6db99
Hemory Phifer <hemoryphifer@quickenloans.com>
2018-04-09 17:03:18 -0400

    Curret progress

---
+++ TreeHouseObjects/Exceptions.cs
@@ -0,0 +1,28 @@
+﻿namespace TreeHouseObjects
+{
+    class TreehouseDefenseException : System.Exception
+    {
+        public TreehouseDefenseException()
+        {
+
+        }
+
+        public TreehouseDefenseException(string message) : base(message)
+        {
+
+        }
+    }
+
+    class OutOfBoundsException : TreehouseDefenseException
+    {
+        public OutOfBoundsException()
+        {
+
+        }
+
+       public OutOfBoundsException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ TreeHouseObjects/Game.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using TreehouseDefense;
+
+namespace TreeHouseObjects
+{
+    partial class Game
+    {
+        static void Main(string[] args)
+        {
+          Map map = new Map();
+
+
+            try
+            {
+
+                Path path = new Path(
+                
+                    new [] {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map),
+                            }
+                );
+
+                MapLocation[] path =
+                {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map),
+                };
+            }
+
+            catch (OutOfBoundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (TreehouseDefenseException)
+            {
+                Console.WriteLine("Unhandled TreehouseDefenseException");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Unhandled Exception");
+
+            }
+
+
+
+        }
+    }
+
+}
